176b38252f11c21c8ba73e783c084c4e73e5d0de
Rajat Kumar <rkumar.cse17@chitkarauniversity.edu.in>
2020-06-08 23:43:27 +0530

    total count corrected

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const _ = require("lodash");
-const mongodb = require("mongodb")
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 const LIMIT = 4;
 const client = new MongoClient(
@@ -8,7 +8,7 @@ const client = new MongoClient(
   "mongodb+srv://rajat:rajat@cluster0-he3og.mongodb.net",
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
+    useUnifiedTopology: true
   }
 );
 client.connect((err) => {
@@ -18,24 +18,35 @@ client.connect((err) => {
   }
   console.log("Connected to MongoDB");
   const users = client.db("fashion").collection("users");
-  router.route("/users")
-    .get(async (req, res) => {
-      const page = req.query.page ? req.query.page : 0;
-      const value = req.query.value
-      //console.log(page)
-      users
-        .find(value ? { $text: { $search: value, $caseSensitive: false } } : {})
-        .sort({ name: 1 })
-        .skip(page * LIMIT)
-        .limit(LIMIT)
-        .toArray((err, fetched_users) => {
-          if (err) {
-            console.log(err);
-            res.send("GET error");
-            return;
-          }
-          users.estimatedDocumentCount((e, n) => res.send({ users: fetched_users, total: n }))
-        });
+  router
+    .route("/users")
+    .get(async ({ query }, res) => {
+      const { page = 0, value } = query;
+      //console.log(page);
+      users.find(
+        value ? { $text: { $search: value, $caseSensitive: false } } : {},
+        (e, fetched_users) => {
+          fetched_users.count((err, n) => {
+            if (err) {
+              console.log(err);
+              res.send("GET count error");
+              return;
+            }
+            fetched_users
+              .sort({ name: 1 })
+              .skip(page * LIMIT)
+              .limit(LIMIT)
+              .toArray((e, users_array) => {
+                if (e) {
+                  console.log(e);
+                  res.send("GET array error");
+                  return;
+                }
+                res.send({ users: users_array, total: n });
+              });
+          });
+        }
+      );
     })
     .post(async (req, res) => {
       users.insertOne(req.body, (err) => {
@@ -45,40 +56,48 @@ client.connect((err) => {
           return;
         }
 
-        res.send("User Added")
+        res.send("User Added");
       });
     })
     .put(async (req, res) => {
-      const { _id, phone, email, name } = req.body
-      users.updateOne({ _id: new mongodb.ObjectId(_id) }, {
-        $set: { email, name, phone }
-      }, (err) => {
-        if (err) { console.log(err); res.send({ err: "Error in updating" }); return; }
-        res.send("User Updated")
-      })
-
+      const { _id, phone, email, name } = req.body;
+      users.updateOne(
+        { _id: new mongodb.ObjectId(_id) },
+        {
+          $set: { email, name, phone }
+        },
+        (err) => {
+          if (err) {
+            console.log(err);
+            res.send({ err: "Error in updating" });
+            return;
+          }
+          res.send("User Updated");
+        }
+      );
     })
     .delete(async (req, res) => {
       users.deleteOne({ _id: new mongodb.ObjectId(req.query._id) }, (err) => {
-
-        if (err) { console.log(err); res.send({ err: "Couldn't delete" }); }
-        else res.send("User deleted");
+        if (err) {
+          console.log(err);
+          res.send({ err: "Couldn't delete" });
+        } else res.send("User deleted");
       });
     });
 
-  router.route("/validate")
-    .post(async (req, res) => {
-      const { field, value } = req.body;
-      users.aggregate([{ $unwind: `$${field}` }, { $match: { [field]: value } }]).toArray((err, fetched_users) => {
+  router.route("/validate").post(async (req, res) => {
+    const { field, value } = req.body;
+    users
+      .aggregate([{ $unwind: `$${field}` }, { $match: { [field]: value } }])
+      .toArray((err, fetched_users) => {
         if (err) {
           res.send({ err: "Unable to find" });
           return;
         }
-        if (fetched_users.length) res.send({ err: `${field} already exists` })
-        else res.send("ok")
-      })
-    })
+        if (fetched_users.length) res.send({ err: `${field} already exists` });
+        else res.send("ok");
+      });
+  });
 });
-
 
 module.exports = router;
